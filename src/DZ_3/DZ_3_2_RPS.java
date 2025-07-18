fdc88fb321cc800338efc2b890e308eec1954e29
nsivyakov <melmanxp@rambler.ru>
2025-07-18 10:31:37 +0300

    Сохранение изменений

--- src/DZ_3/DZ_3_2_RPS.java
+++ src/DZ_3/DZ_3_2_RPS.java
@@ -5,10 +5,48 @@ import java.util.Random;
 public class DZ_3_2_RPS {
     public static void main(String[] args){
         Random random = new Random();
-        int vx = random.nextInt(4); // Назначаем случайное значение Васе
-        int px = random.nextInt(4); // Назначаем случайное значение Пете
-        System.out.println(rps(vx));
+        int vx = random.nextInt(3); // Назначаем случайное значение Васе
+        int px = random.nextInt(3); // Назначаем случайное значение Пете
+        if (vx==0) {
+            if (px==1) {
+                System.out.println("У Васи - " + rps(vx)
+                        + ". У Пети - " + rps(px)
+                        + System.lineSeparator() + "Выиграл Вася!");
+            } else if (px==2) {
+                System.out.println("У Васи - " + rps(vx)
+                        + ". У Пети - " + rps(px)
+                        + System.lineSeparator() + "Выиграл Петя!");
+            }
+        }
+        if (vx==1) {
+            if (px==0) {
+                System.out.println("У Васи - " + rps(vx)
+                        + ". У Пети - " + rps(px)
+                        + System.lineSeparator() + "Выиграл Петя!");
+            } else if (px==2) {
+                System.out.println("У Васи - " + rps(vx)
+                        + ". У Пети - " + rps(px)
+                        + System.lineSeparator() + "Выиграл Вася!");
+            }
+        }
+        if (vx==2) {
+            if (px==0) {
+                System.out.println("У Васи - " + rps(vx)
+                        + ". У Пети - " + rps(px)
+                        + System.lineSeparator() + "Выиграл Вася!");
+            } else if (px==1) {
+                System.out.println("У Васи - " + rps(vx)
+                        + ". У Пети - " + rps(px)
+                        + System.lineSeparator() + "Выиграл Петя!");
+            }
+        }
+        if (vx==px) {
+            System.out.println("У Васи - " + rps(vx)
+                    + ". У Пети - " + rps(px)
+                    + System.lineSeparator() + "Ничья! Нужно играть заново!");
+        }
     }
+
     public static String rps (int var) {
         if (var==0) {
             return "Камень";
--- src/Test/Hello.java
+++ src/Test/Hello.java
@@ -1,5 +1,7 @@
+package Test;
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, people!!!");
+        System.out.println("Test.Hello, people!!!");
     }
 }
